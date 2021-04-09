725bd9b1c9fe2242f3ac42c4dd6e88f542fa8dd7
Eric Johnston <eric.johnston@logicmonkey.ca>
2021-04-09 17:47:44 -0400

    feat: Add dev dependencies and build configs from the base monorepo

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,18 @@
-import { plugins } from '../../rollup.base.config';
-import { getBundleBanner } from '../../scripts/getBundleBanner';
+import { plugins } from "./rollup.base.config";
+import { getBundleBanner } from "./scripts/getBundleBanner";
 
-import pkg from './package.json';
+import pkg from "./package.json";
 
 export default {
-  input: 'src/index.ts',
-  external: ['react', 'react-dom'],
+  input: "src/index.ts",
+  external: ["react", "react-dom"],
   output: {
     globals: {
-      react: 'React',
-      'react-dom': 'ReactDOM',
+      react: "React",
+      "react-dom": "ReactDOM",
     },
-    file: 'dist/umd/index.js',
-    format: 'umd',
+    file: "dist/umd/index.js",
+    format: "umd",
     sourcemap: true,
     name: pkg.name,
     banner: getBundleBanner(pkg),
